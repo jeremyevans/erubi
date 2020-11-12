jeremyevans/erubi

commit d152b2dec928e1050d8c722a207b172bf118f4fb

Reduce memory usage when escaping text

lib/erubi.rb
@@ -168,7 +168,17 @@ module Erubi
 
     # Add raw text to the template
     def add_text(text)
-      @src << " #{@bufvar} << '" << text.gsub(/['\\]/, '\\\\\&') << TEXT_END unless text.empty?
+      @src << " #{@bufvar} << '" << escape_text(text) << TEXT_END unless text.empty?
+    end
+
+    def escape_text(text)
+      if text.frozen?
+        text.gsub(/['\\]/, '\\\\\&')
+      else
+        text['\\'] = '\\\\' if text.include?('\\')
+        text["'"] = "\\'" if text.include?("'")
+        text
+      end
     end
 
     # Add ruby code to the template

commit c78f7226bb83489030a30fdae29ecbe0a04ce640

Fix regression where only first backslash/apostrophe was escaped
d152b2dec928e1050d8c722a207b172bf118f4fb broke pretty much all usage
of Erubi on websites because it only escapes the first backslash
or apostrophe in the text when escaping.  This removes the use
of String#[]= to handle escaping, moving to String#gsub! (or
gsub in the frozen case).

lib/erubi.rb
@@ -179,12 +179,10 @@ module Erubi
     # Add raw text to the template.  Modifies argument if argument is mutable as a memory optimization.
     def add_text(text)
       if text && !text.empty?
-        include_slash = text.include?('\\')
-        include_apos = text.include?("'")
-        if include_slash || include_apos
-          text = text.dup if text.frozen?
-          text['\\'] = '\\\\' if include_slash
-          text["'"] = "\\'" if include_apos
+        if text.frozen?
+          text = text.gsub(/['\\]/, '\\\\\&')
+        else
+          text.gsub!(/['\\]/, '\\\\\&')
         end
         @src << " " << @bufvar << " << '" << text << TEXT_END
       end

test/test.rb
@@ -121,6 +121,49 @@ END2
 END3
   end
 
+  it "should escape all backslashes and apostrophes in text" do
+    list = ['&\'<>"2']
+    check_output(<<END1, <<END2, <<END3){}
+<table>
+ <tbody>' ' \\ \\
+  <% i = 0
+     list.each_with_index do |item, i| %>
+  <tr>
+   <td><%= i+1 %></td>
+   <td><%== item %></td>
+  </tr>
+ <% end %>
+ </tbody>
+</table>
+<%== i+1 %>
+END1
+_buf = ::String.new; _buf << '<table>
+ <tbody>\\' \\' \\\\ \\\\
+';   i = 0
+     list.each_with_index do |item, i| 
+ _buf << '  <tr>
+   <td>'; _buf << ( i+1 ).to_s; _buf << '</td>
+   <td>'; _buf << ::Erubi.h(( item )); _buf << '</td>
+  </tr>
+';  end 
+ _buf << ' </tbody>
+</table>
+'; _buf << ::Erubi.h(( i+1 )); _buf << '
+';
+_buf.to_s
+END2
+<table>
+ <tbody>' ' \\ \\
+  <tr>
+   <td>1</td>
+   <td>&amp;&#39;&lt;&gt;&quot;2</td>
+  </tr>
+ </tbody>
+</table>
+1
+END3
+  end
+
   it "should strip only whitespace for <%, <%- and <%# tags" do
     check_output(<<END1, <<END2, <<END3){}
   <% 1 %>  